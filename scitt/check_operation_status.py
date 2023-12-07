0811fbe530a4dca41356b2783778b4ff4098e408
jgough <joe.gough@datatrails.ai>
2023-12-07 14:17:53 +0000

    fixup - linting and code reshuffle

--- scitt/check_operation_status.py
+++ scitt/check_operation_status.py
@@ -6,17 +6,54 @@ from json import loads as json_loads
 from time import sleep as time_sleep
 
 
-def check_operation_id(
-    operation_id: str
-)-> str:
+# all timeouts and durations are in seconds
+REQUEST_TIMEOUT = 30
+POLL_TIMEOUT = 360
+POLL_INTERVAL = 10
 
-    return subprocess.check_output("curl -s -H @$HOME/.datatrails/bearer-token.txt https://app.datatrails.ai/archivist/v1/publicscitt/operations/" + operation_id, shell=True).decode()
+
+def get_operation_status(operation_id: str) -> str:
+    """
+    gets the operation status from the datatrails API for retrieving operation status
+    """
+
+    # pylint: disable=fixme
+    # TODO: use requests.get, with the request timeout.
+    return subprocess.check_output(
+        # pylint: disable=line-too-long
+        "curl -s -H @$HOME/.datatrails/bearer-token.txt https://app.datatrails.ai/archivist/v1/publicscitt/operations/"
+        + operation_id,
+        shell=True,
+    ).decode()
+
+
+def poll_operation_status(operation_id: str) -> str:
+    """
+    polls for the operation status to be 'succeeded'.
+    """
+
+    poll_attempts: int = int(POLL_TIMEOUT / POLL_INTERVAL)
+
+    for _ in range(poll_attempts):
+        operation_status = get_operation_status(operation_id)
+
+        # pylint: disable=fixme
+        # TODO: ensure get_operation_status handles error cases from the rest request
+        response = json_loads(operation_status)
+        if "status" in response and response["status"] == "succeeded":
+            return response["entryID"]
+
+        time_sleep(POLL_INTERVAL)
+
+    raise TimeoutError("signed statement not registered within polling duration.")
 
 
 def main():
-    """Creates a signed statement"""
+    """Polls for the signed statement to be registered"""
 
-    parser = argparse.ArgumentParser(description="Create a signed statement.")
+    parser = argparse.ArgumentParser(
+        description="Polls for the signed statement to be registered"
+    )
 
     # operation id
     parser.add_argument(
@@ -27,22 +64,8 @@ def main():
 
     args = parser.parse_args()
 
-    # Check the operation status until the status=succeeded
-    # Wait a max of 120 seconds
-    i = 0
-    while i < 120 :
-        retval=check_operation_id(args.operation_id)
-        if retval=="Jwt is expired":
-            print(retval)
-            return
-
-        response = json_loads(check_operation_id(args.operation_id))
-        if "status" in response and response["status"] == "succeeded":
-            print(response["entryID"])
-            break
-
-        time_sleep(1)
-        i+=1
+    entry_id = poll_operation_status(args.operation_id)
+    print(entry_id)
 
 
 if __name__ == "__main__":
