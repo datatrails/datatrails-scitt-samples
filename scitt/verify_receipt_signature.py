b71bb52a2732cf65706cd82c87ef6f85fdf4f5d6
jgough <joe.gough@datatrails.ai>
2024-07-16 09:16:08 +0100

    fixup

--- scitt/verify_receipt_signature.py
+++ scitt/verify_receipt_signature.py
@@ -100,10 +100,10 @@ def verify_receipt(receipt: bytes) -> bool:
     # decode the cbor encoded cose sign1 message
     try:
         message = Sign1Message.decode(receipt)
-    except: 
+    except:
         print("failed to decode cose sign1 receipt", file=sys.stderr)
         return False
-    
+
     # get the verification key from didweb
     kid: bytes = message.phdr[KID]
     didurl = message.phdr[HEADER_LABEL_DID]
