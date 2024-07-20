ae9767283c78c541f237bb0a8d9719a8bcbfbe2d
Jon Geater <jon.geater@rkvst.com>
2024-07-20 15:25:01 -0700

    Nicer formatting in dump_cbor

--- scitt/dump_cbor.py
+++ scitt/dump_cbor.py
@@ -1,7 +1,7 @@
 """ Module for dumping a CBOR file """
 
 import argparse
-
+from pprint import pprint
 from pycose.messages import Sign1Message
 
 def main():
@@ -22,9 +22,13 @@ def main():
     with open(args.input, 'rb') as data_file:
         data = data_file.read()
         message = Sign1Message.decode(data)
-        print(message)
-        print(f'Protected Header: {message.phdr}')
-        print(f'Unprotected Header: {message.uhdr}')
+        print("\ncbor decoded cose sign1 statement:\n")
+        print("protected headers:")
+        pprint(message.phdr)
+        print("\nunprotected headers: ")
+        pprint(message.uhdr)
+        print("\npayload: ", message.payload)
+        print("payload hex: ", message.payload.hex())
 
 if __name__ == "__main__":
     main()
