34e9b7ea9e12d5ccfc120c9617aded14ece81c0d
Jon Geater <jon.geater@rkvst.com>
2024-07-21 15:07:56 -0700

    Upgrade verify script to support Transprent Statement as well as Receipt

--- scitt/verify_receipt_signature.py
+++ scitt/verify_receipt_signature.py
@@ -19,14 +19,22 @@ from pycose.headers import KID
 HEADER_LABEL_DID = 391
 
 
-def open_receipt(receipt_file: str) -> bytes:
+def read_cbor_file(cbor_file: str) -> bytes | None:
     """
     opens the receipt from the receipt file.
     NOTE: the receipt is expected to be in cbor encoding.
     """
-    with open(receipt_file, "rb") as file:
+    with open(cbor_file, "rb") as file:
         receipt = file.read()
-        return receipt
+
+    # decode the cbor encoded cose sign1 message
+    try:
+        message = Sign1Message.decode(receipt)
+    except (ValueError, AttributeError):
+        print("failed to decode cose sign1 from file", file=sys.stderr)
+        return None
+
+    return message
 
 
 def get_didweb_pubkey(didurl: str, kid: bytes) -> dict:
@@ -92,53 +100,81 @@ def get_didweb_pubkey(didurl: str, kid: bytes) -> dict:
     raise ValueError(f"no key with kid: {kid} in verification methods of did document")
 
 
-def verify_receipt(receipt: bytes) -> bool:
+def verify_receipt(receipt: Sign1Message) -> bool:
     """
     verifies the counter signed receipt signature
     """
 
-    # decode the cbor encoded cose sign1 message
-    try:
-        message = Sign1Message.decode(receipt)
-    except (ValueError, AttributeError):
-        print("failed to decode cose sign1 receipt", file=sys.stderr)
-        return False
-
     # get the verification key from didweb
-    kid: bytes = message.phdr[KID]
-    didurl = message.phdr[HEADER_LABEL_DID]
+    kid: bytes = receipt.phdr[KID]
+    didurl = receipt.phdr[HEADER_LABEL_DID]
 
     cose_key_dict = get_didweb_pubkey(didurl, kid)
     cose_key = CoseKey.from_dict(cose_key_dict)
 
-    message.key = cose_key
+    receipt.key = cose_key
 
     # verify the counter signed receipt signature
-    verified = message.verify_signature()
+    verified = receipt.verify_signature()
 
     return verified
+
+
+def verify_transparent_statement(transparent_statement: Sign1Message) -> bool:
+    """
+    verifies the counter signed receipt signature in a TS
+    """
+
+    # Pull the receipt out of the structure
+    try:
+        receipt_bytes = transparent_statement.uhdr["receipts"][0]
+    except (ValueError, AttributeError, KeyError):
+        print("failed to extract receipt from Transparent Statement", file=sys.stderr)
+        return False
+
+    # Re-constitute it as a COSE object
+    try:
+        print(receipt_bytes)
+        receipt = Sign1Message.decode(receipt_bytes)
+    except (ValueError, AttributeError):
+        print("failed to extract receipt from Transparent Statement", file=sys.stderr)
+        return False
+
+    # Verify it
+    print(receipt)
+    return verify_receipt(receipt)
 
 
 def main():
     """Verifies a counter signed receipt signature"""
 
     parser = argparse.ArgumentParser(
-        description="Verify a counter signed receipt signature."
+        description="Verify a counter signed receipt signature from a Receipt or Transparent Statement."
     )
 
-    # signing key file
-    parser.add_argument(
+    options = parser.add_argument_group("Input File Type")
+    options.add_argument(
         "--receipt-file",
         type=str,
-        help="filepath to the stored receipt, in cbor format.",
-        default="scitt-receipt.cbor",
+        help="filepath to a stored Receipt, in CBOR format.",
+    )
+    options.add_argument(
+        "--transparent-statement-file",
+        type=str,
+        help="filepath to a stored Transparent Statement, in CBOR format.",
+        default="transparent-statement.cbor",
     )
 
     args = parser.parse_args()
 
-    receipt = open_receipt(args.receipt_file)
-
-    verified = verify_receipt(receipt)
+    if args.receipt_file:
+        receipt = read_cbor_file(args.receipt_file)
+        verified = verify_receipt(receipt)
+    else:
+        # Note this logic works because only the transparent statement arg
+        # has a default. Don't change that without changing this!
+        transparent_statement = read_cbor_file(args.transparent_statement_file)
+        verified = verify_transparent_statement(transparent_statement)
 
     if verified:
         print("signature verification succeeded")
