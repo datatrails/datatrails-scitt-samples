95a5e1d0bde486ed835201efaf4399bfe46afd21
Paul Hewlett <phewlett76@gmail.com>
2024-11-27 10:16:25 +0000

    Fix merge checks
    Use virtual environement using 'trap deactivate EXIT'
    
    Merging requires that the ci git hub action passes all checks and
    also passes functional tests a.k.a registration-demo.
    
    The github action of releasing must only be executed when releasing as
    wheels in pypi are immutable. Otherwise we would have to bump release
    versions if the releases fail. Once code is merged it is releasable.
    
    Added support for 3.13
    Added additional wheel attributes.

--- tests/constants.py
+++ tests/constants.py
@@ -4,4 +4,4 @@ Unit Test Data Constants
 
 KNOWN_STATEMENT = {"author": "fred", "title": "my biography", "reviews": "mixed"}
 
-KNOWN_RECEIPT_FILE: str = "unittests/resources/scitt-receipt.cbor"
+KNOWN_RECEIPT_FILE: str = "tests/resources/scitt-receipt.cbor"
